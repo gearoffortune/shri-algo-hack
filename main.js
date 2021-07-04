b54f4e806cb16f022fab398b9af7385bbf1a91a2
gearoffortune <hopelessuntildeath@gmail.com>
2021-07-04 14:37:52 +0300

    selection sort

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const array = [5, 3, 1, 8, 7]
+
+function swap(array, pos1, pos2){
+  const t = array[pos1];
+  array[pos1] = array[pos2]
+  array[pos2] = t;
+}
+
 function bubbleSort(array){
 
-  function swap(array, pos1, pos2){
-    const t = array[pos1];
-    array[pos1] = array[pos2]
-    array[pos2] = t;
-  }
   
   for(let i = 0; i < array.length; i++){
     for(let j = 1; j < array.length; j++){
@@ -16,3 +18,25 @@ function bubbleSort(array){
 }
 
 console.log(bubbleSort(array));
+
+const selectionArray = [5, 3, 1, 8, 7, 0, 2]
+
+function selectionSort(arr) {
+  for(let i = arr.length - 1; i !== 0; i--){
+
+    let maxIndex = 0;
+    let max = -Infinity;
+
+    for(let j = 0; j <= i; j++){
+      if(max < arr[j]){
+        max = arr[j];
+        maxIndex = j;
+      }
+    }
+    
+    swap(arr, i, maxIndex);
+  }
+  return arr;
+}
+
+console.log(selectionSort(selectionArray))
